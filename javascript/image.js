09a819a5dd96b12a9180d81f18b700790107b9c9
karishma <kkunwar211@gmail.com>
2020-05-05 11:18:56 +0545

    code for validating the image file extension

--- javascript/image.js
+++ javascript/image.js
@@ -1,16 +1,35 @@
-// image upload
-function readURL(input) {
-    if (input.files) {
+// image upload with image validation
+function readURL() {
+
+    var fileInput = document.getElementById('medicine-image');
+    var filePath = fileInput.value;
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+     if(!allowedExtensions.exec(filePath)){
+        alert('Please upload file having extensions .jpeg/.jpg/.png/.gif only.');
+        fileInput.value = '';
+        return false;
+    }else{
+
+    if (fileInput.files && fileInput.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
             $('#medicine-image-base64').attr('src', e.target.result);
         }
-        reader.readAsDataURL(input.files[0]);
+        reader.readAsDataURL(fileInput.files[0]);
     }
 }
+}
 
-// image update
+// image update with image validation
 function readUpdateURL(input) {
+    var fileInput = document.getElementById('medicine-update-image');
+    var filePath = fileInput.value;
+    var allowedExtensions = /(\.jpg|\.jpeg|\.png|\.gif)$/i;
+     if(!allowedExtensions.exec(filePath)){
+        alert('Please upload file having extensions .jpeg/.jpg/.png/.gif only.');
+        fileInput.value = '';
+        return false;
+    }else{
     if (input.files) {
         var reader = new FileReader();
         reader.onload = function (e) {
@@ -18,6 +37,7 @@ function readUpdateURL(input) {
         }
         reader.readAsDataURL(input.files[0]);
     }
+}
 }
 // event listner for upload and update image
 $("#medicine-image").change(function () {
